b91a124ed627215486146497fa26f070acb02664
Ryan Diel <rdiel01@gmail.com>
2019-01-30 23:20:20 -0600

    add Autowire and save new item to add item controller

--- inventory-manager/src/main/java/com/example/inventorymanager/controllers/InventoryController.java
+++ inventory-manager/src/main/java/com/example/inventorymanager/controllers/InventoryController.java
@@ -2,14 +2,24 @@ package com.example.inventorymanager.controllers;
 
 
 import com.example.inventorymanager.models.Item;
+import com.example.inventorymanager.models.data.ItemDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("inventory")
 public class InventoryController {
+
+    @Autowired
+    private ItemDao itemDao;
 
     @RequestMapping(value="")
     public String index(Model model) {
@@ -24,4 +34,15 @@ public class InventoryController {
         return "inventory/add";
     }
 
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String ProcessAddItemForm(@ModelAttribute @Valid Item newItem, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Item to Inventory");
+            return "inventory/add";
+        }
+
+        itemDao.save(newItem);
+        return "redirect:";
+    }
 }
