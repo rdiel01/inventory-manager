440b5db6e27bccb3cd77f3c6b58a98b13e8473af
Ryan Diel <rdiel01@gmail.com>
2019-01-30 22:33:52 -0600

    add displayAddItemForm

--- inventory-manager/src/main/java/com/example/inventorymanager/controllers/InventoryController.java
+++ inventory-manager/src/main/java/com/example/inventorymanager/controllers/InventoryController.java
@@ -1,9 +1,11 @@
 package com.example.inventorymanager.controllers;
 
 
+import com.example.inventorymanager.models.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("inventory")
@@ -11,7 +13,15 @@ public class InventoryController {
 
     @RequestMapping(value="")
     public String index(Model model) {
+        model.addAttribute("title", "My inventory");
         return "inventory/index";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String displayAddItemForm(Model model) {
+        model.addAttribute("title","Add Item to Inventory");
+        model.addAttribute(new Item());
+        return "inventory/add";
     }
 
 }
