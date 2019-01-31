3c194d0565e3a55f1b1f8a51538bcf9d9bee9316
Ryan Diel <rdiel01@gmail.com>
2019-01-30 22:14:54 -0600

    add entity handler and id

--- inventory-manager/src/main/java/com/example/inventorymanager/models/Category.java
+++ inventory-manager/src/main/java/com/example/inventorymanager/models/Category.java
@@ -1,4 +1,13 @@
 package com.example.inventorymanager.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
 }
