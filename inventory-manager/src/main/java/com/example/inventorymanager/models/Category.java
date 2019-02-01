3de57c7b23f76f27cf4bf1fa07036a5d6f5042ff
Ryan Diel <rdiel01@gmail.com>
2019-01-31 22:08:43 -0600

    add name field

--- inventory-manager/src/main/java/com/example/inventorymanager/models/Category.java
+++ inventory-manager/src/main/java/com/example/inventorymanager/models/Category.java
@@ -3,6 +3,8 @@ package com.example.inventorymanager.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Category {
@@ -10,4 +12,22 @@ public class Category {
     @Id
     @GeneratedValue
     private int id;
+
+    @NotNull
+    @Size(min=3, max=25)
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category() { }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
