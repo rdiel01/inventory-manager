edec3a55d67021a9b18ecd216b365ff36b5fceab
Ryan Diel <rdiel01@gmail.com>
2019-01-31 22:09:19 -0600

    create CategoryDao

---
+++ inventory-manager/src/main/java/com/example/inventorymanager/models/data/CategoryDao.java
@@ -0,0 +1,12 @@
+package com.example.inventorymanager.models.data;
+
+import com.example.inventorymanager.models.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface CategoryDao extends CrudRepository<Category, Integer> {
+}
