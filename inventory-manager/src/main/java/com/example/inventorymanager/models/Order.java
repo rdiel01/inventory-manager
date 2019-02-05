78fee52b198e62fbb31c998a0e57484c5f2f4726
Ryan Diel <rdiel01@gmail.com>
2019-02-05 01:10:03 -0600

    Create Order class

---
+++ inventory-manager/src/main/java/com/example/inventorymanager/models/Order.java
@@ -0,0 +1,48 @@
+package com.example.inventorymanager.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="orders")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    public int id;
+
+    //name is a place holder for creation date
+    @NotNull
+    public String name;
+
+//    @GeneratedValue
+//    private Date createDate;
+//
+//    private Date placedDate;
+//
+//    private Date receivedDate;
+
+    @OneToMany
+    @JoinColumn(name = "order_id")
+    public List<OrderItem> orderItems = new ArrayList<OrderItem>();
+
+    public Order() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List getItems(){ return orderItems;}
+
+    public void addItem(OrderItem item){ orderItems.add(item);}
+}
