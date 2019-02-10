be408765c9723d19eff9f4555e8810472aa2ce00
Ryan Diel <rdiel01@gmail.com>
2019-02-10 12:05:49 -0600

    create form class

---
+++ inventory-manager/src/main/java/com/example/inventorymanager/models/forms/AddOrderItemForm.java
@@ -0,0 +1,46 @@
+package com.example.inventorymanager.models.forms;
+
+
+import com.example.inventorymanager.models.Item;
+import com.example.inventorymanager.models.Order;
+
+import javax.validation.constraints.NotNull;
+
+public class AddOrderItemForm {
+
+    private Order order;
+
+    private Iterable<Item> items;
+
+    @NotNull
+    private int orderId;
+
+    @NotNull
+    private int itemId;
+
+    @NotNull
+    private Integer orderQuantity;
+
+    public AddOrderItemForm() {}
+
+    public AddOrderItemForm(Iterable<Item> items, Order order) {
+        this.order = order;
+        this.items = items;
+    }
+
+    public void setOrderId(int orderId) { this.orderId = orderId;}
+
+    public int getOrderId() { return orderId; }
+
+    public void setItemId(int itemId) { this.itemId = itemId;}
+
+    public int getItemId() { return itemId; }
+
+    public Iterable<Item> getItems() { return items; }
+
+    public Order getOrder() { return order;}
+
+    public void setOrderQuantity(Integer orderQuantity) { this.orderQuantity = orderQuantity;}
+
+    public Integer getOrderQuantity() { return orderQuantity;}
+}
