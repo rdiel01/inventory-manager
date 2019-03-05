7a3ca51b72e907c2312c09da72400ccccec0e66f
Ryan Diel <rdiel01@gmail.com>
2019-03-04 18:48:18 -0600

    add getItemId

--- inventory-manager/src/main/java/com/example/inventorymanager/models/OrderItem.java
+++ inventory-manager/src/main/java/com/example/inventorymanager/models/OrderItem.java
@@ -38,6 +38,8 @@ public class OrderItem {
         return item;
     }
 
+    public int getItemId() { return item.getId(); }
+
     public void setItem(Item item) {
         this.item = item;
     }
